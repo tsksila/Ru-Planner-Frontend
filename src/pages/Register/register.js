97d9fd0a62e5cacf1337df70f1c0b2c1f240e0e5
toey <sila29722@gmail.com>
2021-05-21 21:31:17 +0700

    'fixRegisterPage'

--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -1,33 +1,188 @@
 import React from "react";
-import { useState } from "react";
-import Popup from "../../components/Popup";
-import Controls from "../../controls/Controls";
-import AddIcon from "@material-ui/icons/Add";
-import RegisterForm from "./registerForm";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
+
+
+/*
+const dataForm = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  university: "",
+  faculty: "",
+};
+*/
+
+// Validation with REGEX ^[+-]?\d*(?:[.,]\d*)?$
 
 
 function Register() {
-  
-  const [openPopup, setOpenPopup] = useState(false);
-  return (
-    
-<div> 
-      <Controls.Button
-        text="สมัครสามชิก"
-        variant="outlined"
-        startIcon={<AddIcon />}
-        //className={classes.newButton}
-        onClick={() => setOpenPopup(true)}
-      />
-      <Popup
-        title="สมัครสมาชิก"
-        openPopup={openPopup}
-        setOpenPopup={setOpenPopup}
 
-      >
-        <RegisterForm/>
-      </Popup >
+  const rx_text = /[^0-9]{20}/;
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    criteriaMode: "all",
+  });
+    const onSubmit = (data) => console.log(data);
+  /*
+    const [text, setText] = useState();
+
+  function handleChange(event) {
+    let nam = event.target.name;
+    let val = event.target.value;
+    if (nam === "firstName") {
+        if (val !== "" && !String(val)) {
+            
+        //err = <strong>Your age must be a number</strong>;
+      }
+    }
+   // this.setState({ errors: err });
+  }
+*/
+
+  return (
+    <div className='min-h-screen   bg-blue-100 flex flex-col justify-center  font-base '>
+      <form onSubmit={handleSubmit(onSubmit)}  className='w-9/12  bg-white p-10 mx-auto rounded-3xl'>
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-l    mb-2">   
+                  ชื่อ
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  type="text"
+                  placeholder="สมชาย"
+                  name="firstName"
+                  {...register("firstName", {
+                    required: "This input is required.",
+                    pattern: {
+                      value: rx_text,   /** อันนี้บัคอะ ใส่กรอกอะไรก็ไม่ผ่าน */
+                      message: "กรุณากรอกตัวอักษรเท่านั้น",
+                    },
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  <ErrorMessage
+                    errors={errors}
+                    name="firstName"
+                    render={({ messages }) => {
+                      console.log("messages", messages);
+                      return messages
+                        ? Object.entries(messages).map(([type, message]) => (
+                            <p key={type}>{message}</p>
+                          ))
+                        : null;
+                    }}
+                  />
+                </p>
+              </div>
+
+              <div className="w-full md:w-1/2 px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-l   mb-2">
+                  นามสกุล
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="text"
+                  placeholder="รักเรียน"
+                  {...register("lastName", {
+                    required: true,
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  {errors.lastName?.type === "required" && "กรุณากรอกนามสกุล"}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-l    mb-2">
+                  อีเมล
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  type="email"
+                  placeholder="ramkaum@xxxx.com"
+                  {...register("email", {
+                    required: true,
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  {errors.email?.type === "required" && "กรุณากรอกอีเมล"}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-l    mb-2">
+                  รหัสผ่าน
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="password"
+                  placeholder="******************"
+                  {...register("password", {
+                    required: true,
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  {errors.password?.type === "required" && "กรุณากรอกรหัสผ่าน"}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700  text-l mb-2">
+                  มหาวิทยาลัย
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  type="text"
+                  placeholder="รามคำแหง"
+                  {...register("university", {
+                    required: true,
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  {errors.university?.type === "required" && "กรุณากรอกรหัสผ่าน"}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3">
+                <label className="block uppercase tracking-wide text-gray-700   text-l  mb-2">
+                  คณะ
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="text"
+                  placeholder="วิทยาศาสตร์"
+                  name="faculty"
+                  {...register("faculty", {
+                    required: true,
+                  })}
+                />
+                <p className="text-red-500 text-l italic">
+                  {errors.faculty?.type === "required" && "กรุณากรอกรหัสผ่าน"}
+                </p>
+              </div>
+            </div>
+
+            <div className="md:flex mt-6 justify-end ">
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white rounded-xl py-2 px-4"
+                type="submit"
+              >
+                สมัครสมาชิก
+              </button>
+            </div>
+          </form>
     </div>
+    
   );
 }
 
